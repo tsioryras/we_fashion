d42e9482ed450897bc44cef08b50cce93a6b5529
tsiory Rasamoely <trasamoely@media-sante.com>
2020-06-02 13:45:14 +0200

    initialize seeders

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(User::class)->create(
+            [
+                'name' => 'admin',
+                'email' => 'admin@store.st',
+                'email_verified_at' => now(),
+                'password' => Hash::make('password123'),
+                'remember_token' => Str::random(10),
+            ]
+        );
+    }
+}
