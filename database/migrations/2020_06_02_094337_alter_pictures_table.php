4f13bd7c75836d206baa0e7e2e108acd0e26b960
tsiory Rasamoely <trasamoely@media-sante.com>
2020-06-02 11:45:54 +0200

    addd foreign key product_id to pictures table

---
+++ database/migrations/2020_06_02_094337_alter_pictures_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterPicturesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('pictures', function (Blueprint $table) {
+            $table->unsignedInteger('product_id')->nullable();
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('CASCADE');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('pictures', function (Blueprint $table) {
+            //
+        });
+    }
+}
