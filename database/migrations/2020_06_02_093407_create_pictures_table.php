5300e9a756d2e4538c1ccd0a5cd308c3e15b1938
tsiory Rasamoely <trasamoely@media-sante.com>
2020-06-02 11:42:49 +0200

    create tables pictures

---
+++ database/migrations/2020_06_02_093407_create_pictures_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePicturesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pictures', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('link');
+            $table->string('name');
+            $table->dateTime('updated_at')->nullable(); // DATETIME
+            $table->dateTime('created_at')->nullable(); // DATETIME
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pictures');
+    }
+}
